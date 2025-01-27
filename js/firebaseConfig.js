72c1e5130384da7b12f7d17a2249109eaaeda91f
caballerosdamasdecristo <caballerosdamasdecristo@gmail.com>
2025-01-27 13:51:30 -0600

    Update and rename login.js to firebaseConfig.js

---
+++ js/firebaseConfig.js
@@ -0,0 +1,11 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBSHQifVd2fqWJNPdMQJxLB_wtzBw2wsJI",
+  authDomain: "dbcdc-62a7b.firebaseapp.com",
+  databaseURL: "https://dbcdc-62a7b-default-rtdb.firebaseio.com",
+  projectId: "dbcdc-62a7b",
+  storageBucket: "dbcdc-62a7b.firebasestorage.app",
+  messagingSenderId: "523898093045",
+  appId: "1:523898093045:web:04dd818d3a44790edd74bc",
+  measurementId: "G-QL5HKLGG03"
+};
